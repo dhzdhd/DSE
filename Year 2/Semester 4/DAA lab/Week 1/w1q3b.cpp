8786b18e1e8b302b97679054cab95a8337c1326d
dhzdhd <ragas5432100@gmail.com>
2023-02-06 01:04:47 +0530

    Update program

--- Year 2/Semester 4/DAA lab/Week 1/w1q3b.cpp
+++ Year 2/Semester 4/DAA lab/Week 1/w1q3b.cpp
@@ -1,62 +1,71 @@
 #include <iostream>
 using namespace std;
 
-void addEdge(int **graph, int v1, int v2) {
+void addEdge(int **graph, int v1, int v2)
+{
     graph[v1][v2] = 1;
     graph[v2][v1] = 1;
 }
 
-void display(int **graph, int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+void display(int **graph, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             cout << graph[i][j] << "";
         }
         cout << endl;
     }
 }
 
-int main() {
+int main()
+{
     int choice;
+    int n;
+    cout << "Enter size";
+    cin >> n;
 
-    int input;
-    cout << "Enter number of vertices\n";
-    cin >> input;
+    int **graph = new int *[n];
 
-    const int n = input;
-
-    int graph[n][n];
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++)
+    {
+        graph[i] = new int[n];
+        for (int j = 0; j < n; j++)
+        {
             graph[i][j] = 0;
         }
     }
 
-    cout << "Enter choice\n";
     cout << "1) Insert edge\n";
     cout << "2) Display\n";
 
-    while (true) {
+    while (true)
+    {
         cout << "Enter choice\n";
         cin >> choice;
 
-        switch (choice) {
-            case 1: {
-                int v1, v2;
-                cout << "Enter 2 vertices\n";
-                cin >> v1;
-                cin >> v2;
+        switch (choice)
+        {
+        case 1:
+        {
+            int v1, v2;
+            cout << "Enter 2 vertices\n";
+            cin >> v1;
+            cin >> v2;
 
-                addEdge(graph, v1, v2);
-                break;
-            }
-            case 3: {
-                display(graph, n);
-                break;
-            }
-            default: {
-                exit(0);
-            }
+            addEdge(graph, v1, v2);
+            break;
+        }
+        case 2:
+        {
+            display(graph, n);
+            break;
+        }
+        default:
+        {
+            exit(0);
+        }
         }
     }
 }
