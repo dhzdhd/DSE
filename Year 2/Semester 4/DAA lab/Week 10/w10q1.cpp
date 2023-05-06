fcdaf217943c221cf873f6b27a975b33f85ef9d5
dhzdhd <ragas5432100@gmail.com>
2023-05-06 21:17:39 +0530

    Add warshall algo

---
+++ Year 2/Semester 4/DAA lab/Week 10/w10q1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int **warshall(int **graph, int n) {
+    int **temp = graph;
+
+    for (int k = 1; k < n; k++) {
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                temp[i][j] = temp[i][j] | (temp[i][k] & temp[k][j]);
+            }
+        }
+    }
+
+    return temp;
+}
+
+void display(int **graph, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << graph[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n, e;
+
+    cout << "Enter number of vertices and edges\n";
+    cin >> n >> e;
+
+    int **graph = new int *[n];
+    for (int i = 0; i < n; i++) {
+        graph[i] = new int[n];
+        for (int j = 0; j < n; j++) {
+            graph[i][j] = 0;
+        }
+    }
+
+    cout << "Enter edges & weight\n";
+    for (int i = 0; i < e; i++) {
+        int a, b, w;
+        cin >> a >> b >> w;
+
+        graph[a][b] = w;
+    }
+
+    warshall(graph, n);
+    display(graph, n);
+
+    return 0;
+}
