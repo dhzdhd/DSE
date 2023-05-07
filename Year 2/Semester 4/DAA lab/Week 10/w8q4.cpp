a8296d5bf24019966d0f3d6db9de87e27e34d4e4
dhzdhd <ragas5432100@gmail.com>
2023-05-07 20:56:55 +0530

    Remove unnecessary std::

--- Year 2/Semester 4/DAA lab/Week 10/w8q4.cpp
+++ Year 2/Semester 4/DAA lab/Week 10/w8q4.cpp
@@ -1,61 +1,55 @@
 #include <iostream>
 using namespace std;
-void heapify(int arr[], int n, int i) {
-    int smallest = i; 
-    int l = 2*i + 1; 
-    int r = 2*i + 2; 
 
-    
+void heapify(int arr[], int n, int i) {
+    int smallest = i;
+    int l = 2*i + 1;
+    int r = 2*i + 2;
+
     if (l < n && arr[l] < arr[smallest])
         smallest = l;
 
-     
     if (r < n && arr[r] < arr[smallest])
         smallest = r;
 
-   
     if (smallest != i) {
-        std::swap(arr[i], arr[smallest]);
-
-       
+        swap(arr[i], arr[smallest]);
         heapify(arr, n, smallest);
     }
 }
 
 void deleteMin(int arr[], int& n) {
     if (n == 0) {
-        std::cout << "Heap is empty!" << std::endl;
+        cout << "Heap is empty!" << endl;
         return;
     }
-    
+
     arr[0] = arr[n-1];
     n--;
 
-    
     heapify(arr, n, 0);
 }
 
 int main() {
     int arr[100];
     int n;
-    cout<<"Enter the number of elements \n";
-    cin>>n;
-    cout<<"Enter elements\n";
+
+    cout << "Enter the number of elements \n";
+    cin >> n;
+    cout << "Enter elements\n";
+
     for(int i = 0;i<n;i++){
         cin>>arr[i];
     }
 
-    
     for (int i = n/2 - 1; i >= 0; i--)
         heapify(arr, n, i);
 
-    
     deleteMin(arr, n);
 
-    
     for (int i = 0; i < n; ++i)
-        std::cout << arr[i] << " ";
-    std::cout << std::endl;
+        cout << arr[i] << " ";
+    cout << endl;
 
     return 0;
 }
