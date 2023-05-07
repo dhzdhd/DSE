1a1dc9c3c36669dcaeee002796260d7dd41131c6
SieGe0701 <116585928+SieGe0701@users.noreply.github.com>
2023-05-07 20:21:17 +0530

    add w10q1 and q4

--- Year 2/Semester 4/DAA lab/Week 10/w10q4.cpp
+++ Year 2/Semester 4/DAA lab/Week 10/w10q4.cpp
@@ -1,56 +1,54 @@
-// Knapsack 0/1
-
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-int mfks(int i, int W, int *weights, int *values, int matrix[100][100]) {
-    if (matrix[i][W] < 0) {
-        int value;
-        if (W < weights[i]) {
-            value = mfks(i - 1, W, weights, values, matrix);
-        } else {
-            value = max(
-                mfks(i - 1, W, weights, values, matrix),
-                values[i] + mfks(i - 1, W - weights[i], weights, values, matrix)
-            );
-        }
-
-        matrix[i][W] = value;
-    }
-    return mfks(i, W, weights, values, matrix);
+#define max 100
+int weights[max];
+int values[max];
+int n;
+int w;
+int **f = new int *[n+1];
+int maxi(int a,int b){
+    int maxi = a>b?a:b;
+    return maxi;
 }
+void funcIni(){
+    for(int i = 0;i<n+1;i++){
+        f[i] = new int[w+1];
+    }
+    for(int i = 0;i<n+1;i++){
+        for(int j = 0;j<w+1;j++){
+            if(i==0 || j==0)
+            f[i][j]=0;
+            else
+            f[i][j]=-1;
+        }
+    }
+}
+int knapsack() {
+    int dp[n+1][w+1];
 
-int main() {
-    int matrix[100][100];
-    int n, W;
-
-    cout << "Enter number of values & knapsack capacity\n";
-    cin >> n >> W;
-
-    int *weights = new int[n];
-    int *values = new int[n];
-
-    cout << "Enter weights and values\n";
-    for (int i = 0; i < n; i++) {
-        cin >> weights[i] >> values[i];
-
-        for (int j = 0; j < W; j++) {
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= w; j++) {
             if (i == 0 || j == 0) {
-                matrix[i][j] = 0;
+                dp[i][j] = 0;
+            } else if (weights[i-1] <= j) {
+                dp[i][j] = maxi(values[i-1] + dp[i-1][j-weights[i-1]], dp[i-1][j]);
             } else {
-                matrix[i][j] = -1;
+                dp[i][j] = dp[i-1][j];
             }
         }
     }
 
-    mfks(n - 1, W, weights, values, matrix);
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < W; j++) {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
+    return dp[n][w];
+}
+int main(){
+    cout<<"Enter the number of items \n";
+    cin>>n;
+    for(int i = 0; i<n;i++){
+        cout<<"Enter the weight and values of the item "<<i+1<<"\n";
+        cin>>weights[i]>>values[i];
     }
-
+    cout<<"Enter the capacity of the knapsack \n";
+    cin>>w;
+    cout<<"Max value: \n"<<knapsack();
     return 0;
 }
