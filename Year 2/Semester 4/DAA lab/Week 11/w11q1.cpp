701c6f19dc8f304a387abf70fb66d193dcd94fdb
dhzdhd <ragas5432100@gmail.com>
2023-05-07 17:20:48 +0530

    Add broken prim

--- Year 2/Semester 4/DAA lab/Week 11/w11q1.cpp
+++ Year 2/Semester 4/DAA lab/Week 11/w11q1.cpp
@@ -3,8 +3,74 @@
 #include <iostream>
 using namespace std;
 
+bool in(int vertex, int *vertices, int n) {
+    for (int i = 0; i < n; i++) {
+        if (vertex == vertices[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void prim(int **graph, int n, int e, int *vertices, int **edges) {
+    int cv = 0;
+    vertices[cv++] = 0;
+
+    for (int i = 0; i < cv; i++) {
+        int start = vertices[i];
+
+        int min = 100000;
+        int j;
+
+        for (j = 0; j < n && !in(j, vertices, cv); j++) {
+            int edge = graph[start][j];
+
+            if (edge != 0 && edge < min) {
+                min = edge;
+            }
+        }
+
+        vertices[cv++] = j;
+        edges[start][j] = min;
+    }
+}
+
 int main() {
-    int **graph = new int *[10];
+    int n, e;
+
+    cout << "Enter number of vertices and edges\n";
+    cin >> n >> e;
+
+    int **graph = new int *[n];
+    int **edges = new int *[n];
+    int *vertices = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        graph[i] = new int[n];
+        edges[i] = new int[n];
+        for (int j = 0; j < n; j++) {
+            graph[i][j] = 0;
+            edges[i][j] = 0;
+        }
+    }
+
+    cout << "Enter edges & value\n";
+    for (int i = 0; i < e; i++) {
+        int a, b, val;
+        cin >> a >> b >> val;
+        graph[a][b] = val;
+        graph[b][a] = val;
+    }
+
+    prim(graph, n, e, vertices, edges);
+
+    cout << "MST is\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << edges[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
