cbe532c469958ec5ea30a6155fc612d380a7de00
dhzdhd <ragas5432100@gmail.com>
2023-08-28 22:51:29 +0530

    Add OS W3 exercise 9

--- Year 3/Semester 5/OS lab/Week 3/exercises/9.sh
+++ Year 3/Semester 5/OS lab/Week 3/exercises/9.sh
@@ -1,14 +1,26 @@
 # !/bin/bash
 
-read -p "Enter file" f
+for f in "$@"; do
+    if [ -e "$f" ]; then
+        if [ -f "$f" ]; then
+            echo "$f is an ordinary file."
+            echo "Content of the file:"
+            cat "$f"
 
-if [ -f $f ] || [ -d $f ]; then
-    if [ -f $f ]; then
-        $(cat $f)
+            if [ -x "$f" ]; then
+                echo "$f has execute permission."
+            else
+                echo "$f does not have execute permission."
+            fi
+
+        elif [ -d "$f" ]; then
+            echo "$f is a directory."
+
+            num_files=$(ls -1 "$f" | wc -l)
+            echo "Number of files in the directory: $num_files"
+        fi
 
     else
-
+        echo "$f does not exist."
     fi
-else
-    echo "Does not exist"
-fi
+done
