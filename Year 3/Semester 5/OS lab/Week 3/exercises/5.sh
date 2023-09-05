027082cab6e28de830e349585d9d372d1090143f
dhzdhd <ragas5432100@gmail.com>
2023-09-06 00:54:53 +0530

    Complete W2 DL

--- Year 3/Semester 5/OS lab/Week 3/exercises/5.sh
+++ Year 3/Semester 5/OS lab/Week 3/exercises/5.sh
@@ -4,7 +4,7 @@ read -p "Enter file name " f
 read -p "Number of lines " num
 read -p "Enter head(h) or tail(t) " ht
 
-if [ -f f ]; then
+if [ -f $f ]; then
     if [[ $ht == "h" ]]; then
         head -$num $f
     else
