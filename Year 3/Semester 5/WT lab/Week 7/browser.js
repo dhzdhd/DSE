b5ad6d42c89a8a7a11b8337a23bd72e8dff80455
dhzdhd <ragas5432100@gmail.com>
2023-09-20 12:17:52 +0530

    Fix WT W7 browser

--- Year 3/Semester 5/WT lab/Week 7/browser.js
+++ Year 3/Semester 5/WT lab/Week 7/browser.js
@@ -3,7 +3,13 @@ let windowObjectReference = null;
 function openRequestedTab(url, windowName) {
   if (windowObjectReference === null || windowObjectReference.closed) {
     windowObjectReference = window.open("", "new");
+    windowObjectReference.document.write("<h1>Hello, world!</h1>");
   } else {
     windowObjectReference.focus();
   }
 }
+
+const button = document.querySelector(".button");
+button.addEventListener("click", () => {
+  openRequestedTab("https://www.example.com", "new");
+});
