3e8be0bc552e1e901bc18397416e27fe8b338a1d
dhzdhd <ragas5432100@gmail.com>
2024-01-22 12:30:46 +0530

    Remove np dep

--- Year 3/Semester 6/BDA lab/Week 2/2/mul_reducer.py
+++ Year 3/Semester 6/BDA lab/Week 2/2/mul_reducer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import sys
-import numpy as np
 
 
 def main():
@@ -9,6 +8,7 @@ def main():
 
     m1 = [[0] * 3 for _ in range(3)]
     m2 = [[0] * 3 for _ in range(3)]
+    m3 = [[0] * 3 for _ in range(3)]
     for line in raw:
         matrix, pos, value = line.split("\t")
         match matrix:
@@ -17,7 +17,11 @@ def main():
             case "b":
                 m2[int(pos[0])][int(pos[-1])] = int(value)
 
-    m3 = np.matmul(m1, m2)
+    for i in range(len(m1[0])):
+        for j in range(len(m2)):
+            for k in range(len(m1)):
+                m3[i][j] += m1[i][k] * m2[k][j]
+
     for l in range(len(m3)):
         for i in range(len(m3[l])):
             print(f"c\t{l},{i}\t{m3[l][i]}")
