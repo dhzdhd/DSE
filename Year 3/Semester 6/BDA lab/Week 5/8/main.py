7e846d4de978ec992a6d0afd2616f9c2ca538f3d
dhzdhd <ragas5432100@gmail.com>
2024-02-25 13:24:36 +0530

    Complete W5 BDA

--- Year 3/Semester 6/BDA lab/Week 5/8/main.py
+++ Year 3/Semester 6/BDA lab/Week 5/8/main.py
@@ -3,14 +3,28 @@ import pyspark
 sc = pyspark.SparkContext()
 rdd = sc.textFile("./data.csv")
 
-rddSort = rdd.map(lambda row: row.split(",")).sortByKey()
-rddGroup = rdd.map(lambda row: row.split(",")).groupByKey()
-rddCount = rdd.map(lambda row: row.split(",")).countByKey()
+
+def getPair(xs: list[str]):
+    key = xs[2]
+    val = xs[:1] + xs[3:]
+    return (key, val)
+
+
+rddSort = (
+    rdd.map(lambda row: row.split(",")).map(lambda xs: (xs[0], xs[1:])).sortByKey()
+)
+rddGroup = (
+    rdd.map(lambda row: row.split(","))
+    .map(getPair)
+    .groupByKey()
+    .map(lambda t: (t[0], t[1].data))
+)
+rddCount = rdd.map(lambda row: row.split(",")).map(getPair).countByKey()
 
 print(f"Sort: {rddSort.collect()}")
 print(f"Group: {rddGroup.collect()}")
-print(f"Count: {rddCount.collect()}")
+print(f"Count: {rddCount.items()}")
 
-# rddSort.saveAsTextFile("./sortout")
-# rddGroup.saveAsTextFile("./groupout")
-# rddCount.saveAsTextFile("./countout")
+rddSort.saveAsTextFile("./sortout")
+rddGroup.saveAsTextFile("./groupout")
+rddCount.saveAsTextFile("./countout")
