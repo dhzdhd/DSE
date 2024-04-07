a3d176d4dfc030b55c7a2a59ea15c81c3e9d67be
dhzdhd <ragas5432100@gmail.com>
2024-04-07 11:51:40 +0530

    Complete W7 BDA

---
+++ Year 3/Semester 6/BDA lab/Week 7/2/main.scala
@@ -0,0 +1,17 @@
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
+val df = spark.read
+  .format("csv")
+  .option("header", "true")
+  .option("inferSchema", "true")
+  .option("timestampFormat", "yyyy-MM-dd HH:mm:ss")
+  .load("data.csv")
+
+val finalDf = df.withColumn("timestamp", col("timestamp").cast(TimestampType))
+finalDf
+  .groupBy("user_id")
+  .agg(max("timestamp").alias("max"), min("timestamp").alias("min"))
+  .select($"user_id", $"max" - $"min")
+  .orderBy("user_id")
+  .show()
